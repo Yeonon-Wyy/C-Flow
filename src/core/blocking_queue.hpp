5dd66d17e2d037e73fde7a25dad14e1c6bd13cc5
Weiyanyu <chinayeonon@gmail.com>
2021-10-20 19:47:16 +0800

    修改文件结构

---
+++ src/core/blocking_queue.hpp
@@ -0,0 +1,15 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: yeonon
+ * @Date: 2021-10-16 22:00:26
+ * @LastEditors: yeonon
+ * @LastEditTime: 2021-10-16 22:02:59
+ */
+namespace vtf {
+class BlockingQueue
+{
+    //TODO：改天实现
+};
+
+} //namespace vtf
